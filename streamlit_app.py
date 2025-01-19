724e7dc7da92e1e91f5718a3510870930cbdf5e4
Avinash Venugopal <avinashvenugopal96@gmail.com>
2025-01-19 17:03:01 +0530

    Update streamlit_app.py
    adding requests import

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import os
 from pprint import pprint
+import requests
 from bs4 import BeautifulSoup as soup
 
 def prettify_raw_html(html_string, engine='bs4'):
